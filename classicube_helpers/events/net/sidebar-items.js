492e8e2006d00baf9d6b1fbcf93b1f6fc64adb35
SpiralP <SpiralP@users.noreply.github.com>
2022-01-12 21:36:24 +0000

    deploy: 03179d85fc2ee602ac083704c13688f73ce21642

--- classicube_helpers/events/net/sidebar-items.js
+++ classicube_helpers/events/net/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ConnectedEvent",""],["ConnectedEventHandler","Connection to a server was established."],["DisconnectedEvent",""],["DisconnectedEventHandler","Connection to the server was lost."]]});
+initSidebarItems({"struct":[["ConnectedEvent",""],["ConnectedEventHandler","Connection to a server was established."],["DisconnectedEvent",""],["DisconnectedEventHandler","Connection to the server was lost."],["PluginMessageReceivedEvent",""],["PluginMessageReceivedEventHandler","Connection to the server was lost."]]});
